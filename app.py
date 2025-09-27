507ce62ca4d1dc63da9f751f815ef588615db841
mehdiff676-dev <mehdiff676@gmail.com>
2025-09-27 20:10:22 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -684,7 +684,7 @@ if __name__ == "__main__":
         print("No accounts file found. Starting without preloaded accounts.")
 
     try:
-        app.run(host='0.0.0.0', port=15028, debug=False)
+        app.run(host='0.0.0.0', port=5000, debug=False)
     except KeyboardInterrupt:
         print("Server stopped by user")
         cleanup()
